89803a2c9a9d1077c6fee5090de1c6fecfa3ec80
Tekkkclkdz <antekkorzecc@gmail.com>
2025-04-12 12:14:03 +0200

    april upgrades 2

--- src/app/bio/page.tsx
+++ src/app/bio/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { FaInstagram } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi'; // Nowa ikona maila
+import { HiOutlineMail } from 'react-icons/hi';
 import bioImage from '../../../public/public2/bio.jpg';
 import NavBar from '../components/NavBar';
 
@@ -16,9 +16,9 @@ export default function Home() {
       <div className="w-full max-w-3xl text-left leading-relaxed mb-10">
         <h2 className="text-3xl font-bold mb-4">VISUAL</h2>
         <p>
-          Swu's interest in image and cinema was first sparked by his older brother, photographer and filmmaker
+          Swu&#39;s interest in image and cinema was first sparked by his older brother, photographer and filmmaker
           <a href="https://www.mathiaskarlgontard.com/" target="_blank" className="underline"> Mathias Karl</a>, who exposed him to intricate camera techniques, thought-provoking films, and visually complex
-          compositions early on in his life. The Swu's photography, often relying on raw form, structure, as well as heavy
+          compositions early on in his life. The Swu&#39;s photography, often relying on raw form, structure, as well as heavy
           play of light and shadow, reflects his experience of growing up in rapidly developing post-Soviet Poland and a
           desolate, industrial area in the south side of its capital city, Warsaw.
         </p>
@@ -31,7 +31,7 @@ export default function Home() {
         </p>
         <p className="mt-4">
           That same area, home to the Tor Wyścigów Konnych Służewiec horse racing track, placed history directly within
-          the reach of his palm. The Nazis used it as an airbase for their air fleet between 1939 and '45. A water pressure
+          the reach of his palm. The Nazis used it as an airbase for their air fleet between 1939 and &#39;45. A water pressure
           tower, bombed down in an act of unprovoked destruction upon their retreat, in a way like few of its kind, to this
           day, lies down the road from his childhood home. The entire length of the perimeter wall running in front of his
           window is the site of many executions and features countless bullet casings from the Warsaw uprising.
@@ -39,10 +39,10 @@ export default function Home() {
 
         <h2 className="text-3xl font-bold mt-8 mb-4">SOUND</h2>
         <p>
-          Swu's first experience with music dates back to second grade when he had enrolled in music classes at his primary
+          Swu&#39;s first experience with music dates back to second grade when he had enrolled in music classes at his primary
           school and was taught the fundamentals of rhythm. This caused him to take up vocal and piano classes with Polish
           Opera Singer <a href="https://adrianaferfecka.com/" target="_blank" className="underline">Adriana Ferfecka</a> and Warsaw Conservatory Music Theory Professor <a href="https://chopin.edu.pl/employees/142_ewa-guz-seroka" target="_blank" className="underline"> Ewa Guz-Seroka</a> and later pursue
-          formal education in music, studying <strong>Production & Audio Engineering</strong> at <strong>Point Blank Music School</strong> in London.
+          formal education in music, studying <strong>Production &amp; Audio Engineering</strong> at <strong>Point Blank Music School</strong> in London.
         </p>
         <p className="mt-4">
           He had begun experimenting with his own production at the age of 12, initially splicing Polish folk songs, often
@@ -53,9 +53,9 @@ export default function Home() {
         <p className="mt-4">
           In a life full of <strong>contrasts</strong> and <strong>contradictions</strong>, he pursues <strong>clarity</strong> through <strong>abstraction</strong> -
         </p>
-        <p className="italic mt-4">"Creating art was always <strong>expression</strong>. A means of making sense of things. A way of processing,
+        <p className="italic mt-4">&quot;Creating art was always <strong>expression</strong>. A means of making sense of things. A way of processing,
           translating, creating structure where there was once only feeling - of <strong>acknowledging, reflecting, highlighting</strong>
-          and <strong>affirming</strong>, using techniques that resonated most in the current moment..."
+          and <strong>affirming</strong>, using techniques that resonated most in the current moment...&quot;
         </p>
 
         <h2 className="text-3xl font-bold mt-8 mb-4">LITERARY</h2>
@@ -70,12 +70,12 @@ export default function Home() {
           seemingly instigated by impulses reflecting rhythms of his mindstate, rather than any <strong>sense of time and tempo</strong>.
         </p>
         <p className="mt-4">
-          Swu's name can currently only be linked to a limited number of broadly available projects. Looking to pursue
+          Swu&#39;s name can currently only be linked to a limited number of broadly available projects. Looking to pursue
           various areas of his interest while being rigorously aware of his young age, the artist has chosen to mostly keep
           his work private over the years.
         </p>
-        <p className="mt-4 italic">"I want to be a better person in every aspect. I really don't feel I've in any way fulfilled my
-          potential in every area of my life. But I'm optimistic."
+        <p className="mt-4 italic">&quot;I want to be a better person in every aspect. I really don&#39;t feel I&#39;ve in any way fulfilled my
+          potential in every area of my life. But I&#39;m optimistic.&quot;
         </p>
         <p className="text-right font-bold">Damon Albarn</p>
       </div>
